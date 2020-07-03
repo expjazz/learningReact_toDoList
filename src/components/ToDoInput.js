53888ed1230119a1ec32a69fce4988fbe2b03c96
Expedito Andrade <expeditojazz@gmail.com>
2020-07-03 07:56:49 -0300

    add todo styling. Decompose todo props

--- src/components/ToDoInput.js
+++ src/components/ToDoInput.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react'
 
 export default class ToDoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props
     return (
-      <div>
-        Hello from the todo input
-      </div>
+      <div className='card card-body my-3'>
+        <form onSubmit={handleSubmit}>
+          <div className="inputgroup">
+
+          <div className="input-group-prepend">
+            <div className="input-group-text bg-primary text-white">
+              <i className="fas fa-book"></i>
+            </div>
+          </div>
+          <input type="text" className="form-control text-capitalize" placeholder='add todo item' 
+          value={item}
+          onChan={handleChange}/>
+
+          </div>
+          <button type='submit' className="btn btn-block btn-primary mt-3 text-uppercase">add item</button>
+          </form>
+     </div>
     )
   }
 }
