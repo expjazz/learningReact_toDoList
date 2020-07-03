22a24309dbe8df70f6a1df4d057fee19e25abf9d
Expedito Andrade <expeditojazz@gmail.com>
2020-07-03 07:00:04 -0300

    initial setup

---
+++ src/components/ToDoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ToDoInput extends Component {
+  render() {
+    return (
+      <div>
+        Hello from the todo input
+      </div>
+    )
+  }
+}
